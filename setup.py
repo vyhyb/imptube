87f926844482114ca272df43d7ad5a3c1002779d
vyhyb <zahybek@gmail.com>
2024-03-13 16:35:34 +0100

    bug fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.md", "r") as f:
 
 setup(
     name= "imptube",
-    version= "0.0.3",
+    version= "0.0.4",
     description = "A package for impedance tube measurements.",
     package_dir={"": "imptube"},
     packages=find_packages(where="imptube"),
