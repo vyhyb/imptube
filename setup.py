75f9c5e8d681dd7193777fa5ce048f9d7d04c110
vyhyb <zahybek@gmail.com>
2025-02-14 13:57:48 +0100

    bump version to 0.0.7 in setup.py and PKG-INFO

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.md", "r") as f:
 
 setup(
     name= "imptube",
-    version= "0.0.6",
+    version= "0.0.7",
     description = "A package for impedance tube measurements.",
     package_dir={"": "imptube"},
     packages=find_packages(where="imptube"),
