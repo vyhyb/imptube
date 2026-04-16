acda0f619f8e0548f3ff90fed3e515b488016cfa
vyhyb <zahybek@gmail.com>
2026-04-16 11:16:55 +0200

    reimplement the files save and load

--- imptube/processing/files.py
+++ imptube/processing/files.py
@@ -1,510 +1,98 @@
-"""
-This module contains functions for reading and processing audio files.
-
-!!! This module became obsolete after the reimplementation of the tube.py module, 
-!!! it is not usable for the current version of the code. It is kept here for reference.
-"""
-import scipy.io.wavfile as wav
-import pandas as pd
-import numpy as np
-import os
-from scipy.fft import fftfreq
-
-from .signal_proc import (
-    read_audio,
-    stereo_to_spectra,
-    transfer_function, 
-    calibration_factor, 
-    reflection_factor, 
-    absorption_coefficient,
-    surface_impedance,
-    tf_i_r,
-)
-from .filters import noise_filtering
-from ..utils import filter, read_folder
-
-
-def read_file(path : str) -> tuple[int, np.ndarray]:
-    """
-    Read a WAV file from the given path.
-
-    Parameters
-    ----------
-    path : str
-        The path to the WAV file.
-
-    Returns
-    -------
-    tuple
-        A tuple containing two elements:
-        - The audio data as a 1-D numpy array.
-        - The sample rate of the audio.
-
-    """
-    return wav.read(path)
-
-import numpy as np
-import pandas as pd
-
-def load_bc(parent_folder : str) -> pd.DataFrame:
-    """
-    Load the bound condition data from a given parent folder.
-
-    Parameters:
-    -----------
-    parent_folder : str
-        The path to the parent folder containing the bound condition file.
-
-    Returns:
-    --------
-    pd.DataFrame
-        The loaded bound condition data as a pandas DataFrame.
-    """
-    file_paths = read_folder(parent_folder)
-    bc_path = filter(
-            file_paths,
-            ["bound_cond.csv"]
-    )
-    bc_df = pd.read_csv(bc_path[0])
-    return bc_df
-
-def load_freqs(parent_folder : str) -> np.ndarray:
-    """
-    Load frequency data from the specified parent folder.
-
-    Parameters
-    ----------
-    parent_folder : str
-        The path to the parent folder containing the frequency data.
-
-    Returns
-    -------
-    numpy.ndarray
-        The loaded frequency data as a NumPy array.
-    """
-    file_paths = read_folder(parent_folder)
-    freqs_path = filter(
-            file_paths,
-            ["freqs.npy"]
-    )
-    freqs = np.load(freqs_path[0])
-    return freqs
+from dataclasses import dataclass
+from pathlib import Path
 
 import numpy as np
 
-def load_alpha(parent_folder : str) -> tuple[list[np.ndarray], list[str]]:
-    """Load sound absorption coefficient data from the specified parent folder.
+@dataclass
+class EnvironmentalConditions:
+    temperature: float       # °C
+    rel_humidity: float      # %
+    atm_pressure: float = 101325.0  # Pa
 
-    Parameters
-    ----------
-    parent_folder : str
-        The path to the parent folder containing the sound absorption coefficient data files.
 
-    Returns
-    -------
-    tuple
-        A tuple containing two lists:
-        - alpha: A list of numpy arrays, each representing an sound absorption coefficient data file.
-        - unique_d: A list of unique 'd' values extracted from the file paths.
-    """
-    file_paths = read_folder(parent_folder)
-    alpha_path = filter(
-            file_paths,
-            ["_alpha"]
-    )
-    alpha = []
-    unique_d = []
-    for p in alpha_path:
-        alpha.append(np.load(p))
-        idx1 = p.rfind("d")
-        idx2 = p.rfind(".")
-        unique_d.append(p[idx1:idx2])
-    return alpha, unique_d
+@dataclass
+class BoundaryConditions:
+    further_mic_dist: float  # m, distance of the further mic from the sample
+    closer_mic_dist: float   # m, distance of the closer mic from the sample
+    freq_limit: float        # Hz, upper frequency limit of the measurement
 
-def transfer_function_from_file(
-        path : str=None, 
-        folder : str=None, 
-        filter_str : str="_"
-        ) -> np.ndarray:
-    """Calculate the transfer function from a file or a folder of files.
 
-    Parameters
-    ----------
-    path : str, optional
-        Path to the file, by default None.
-    folder : str, optional
-        Path to the folder containing files, by default None.
-    filter_str : str, optional
-        Filter string to select specific files in the folder, by default "_".
+@dataclass
+class MeasurementResults:
+    freqs: np.ndarray                           # Hz
+    alpha: np.ndarray                           # absorption coefficient
+    reflection_factor: np.ndarray | None = None # complex reflection factor
+    surface_impedance: np.ndarray | None = None # complex surface impedance (Pa·s/m)
+    env: EnvironmentalConditions | None = None
+    bc: BoundaryConditions | None = None
 
-    Returns
-    -------
-    ndarray
-        The transfer function calculated from the input file(s).
+    def save(self, path: str) -> None:
 
-    Raises
-    ------
-    ValueError
-        If both `path` and `folder` are None.
+        path = Path(path)
+        if path.suffix != ".npz":
+            path = path.with_suffix(".npz")
 
-    Notes
-    -----
-    - If `folder` is None, the function will calculate the transfer function from a single file specified by `path`.
-    - If `folder` is provided, the function will calculate the average transfer function from all files in the folder that match the `filter_str`.
+        data = {
+            "freqs": self.freqs,
+            "alpha": self.alpha,
+            "has_reflection_factor": self.reflection_factor is not None,
+            "has_surface_impedance": self.surface_impedance is not None,
+            "has_env": self.env is not None,
+            "has_bc": self.bc is not None,
+        }
 
-    """
-    if folder is None:
-        if path is None:
-            raise ValueError("You need to define path to file (path), or to folder (folder)")
-        else:
-            f = read_file(path)
-            a = read_audio(f)
-            p1, p2 = stereo_to_spectra(a)
-            tf = transfer_function(p1, p2)
-    else:
-        files = read_folder(folder)
-        f_paths = filter(files, [filter_str])
-        f = []
-        a = []
-        p1 = []
-        p2 = []
-        for fp in f_paths:
-            f.append(read_file(fp))
-            a.append(read_audio(f[-1]))
-            p = stereo_to_spectra(a[-1])
-            p1.append(p[0])
-            p2.append(p[1])
-        p1_np = np.array(p1)
-        p2_np = np.array(p2)
-        p1_np = p1_np.mean(axis=0)
-        p2_np = p2_np.mean(axis=0)
-        tf = transfer_function(p1_np, p2_np)
-    return tf
+        if self.reflection_factor is not None:
+            data["reflection_factor"] = self.reflection_factor
+        if self.surface_impedance is not None:
+            data["surface_impedance"] = self.surface_impedance
+        if self.env is not None:
+            data["env_temperature"] = self.env.temperature
+            data["env_rel_humidity"] = self.env.rel_humidity
+            data["env_atm_pressure"] = self.env.atm_pressure
+        if self.bc is not None:
+            data["bc_further_mic_dist"] = self.bc.further_mic_dist
+            data["bc_closer_mic_dist"] = self.bc.closer_mic_dist
+            data["bc_freq_limit"] = self.bc.freq_limit
 
-def calibration_from_files(
-        path1: str = None,
-        path2: str = None, 
-        parent_folder: str = None, 
-        export: bool = True,
-        noise_filter: bool = False
-        ) -> tuple[np.ndarray, np.ndarray]:
-    """
-    Perform calibration from audio files.
+        np.savez_compressed(path, **data)
 
-    Parameters
-    ----------
-    path1 : str, optional
-        Path to the first audio file, by default None.
-    path2 : str, optional
-        Path to the second audio file, by default None.
-    parent_folder : str, optional
-        Path to the parent folder containing calibration and audio folders, by default None.
-    export : bool, optional
-        Whether to export calibration data, by default True.
+    @classmethod
+    def load(cls, path: str) -> "MeasurementResults":
 
-    Returns
-    -------
-    tuple[np.ndarray, np.ndarray]
-        Tuple containing the calibration factor and frequency data.
-    """
-    if parent_folder is None:
-        f1 = read_file(path1)
-        f2 = read_file(path2)
-        a1 = read_audio(f1)
-        a2 = read_audio(f2)
-        p11, p12 = stereo_to_spectra(a1)
-        p21, p22 = stereo_to_spectra(a2)
-        cf = calibration_factor(p11, p12, p21, p22)
-    else:
-        folder = os.path.join(parent_folder, "calibration", "audio")
-        export_folder = os.path.join(parent_folder, "calibration", "cal_factor")
-        files = read_folder(folder)
-        f1_paths = filter(files, ["conf1"])
-        f2_paths = filter(files, ["conf2"])
-        f1 = []
-        f2 = []
-        a1 = []
-        a2 = []
-        p11 = []
-        p12 = []
-        p21 = []
-        p22 = []
-        for f_1, f_2 in zip(f1_paths, f2_paths):
-            f1.append(read_file(f_1))
-            f2.append(read_file(f_2))
-            a1.append(read_audio(f1[-1]))
-            a2.append(read_audio(f2[-1]))
-            p1 = stereo_to_spectra(a1[-1])
-            p2 = stereo_to_spectra(a2[-1])
-            p11.append(p1[0])
-            p12.append(p1[1])
-            p21.append(p2[0])
-            p22.append(p2[1])
-        p11_np = np.array(p11)
-        p12_np = np.array(p12)
-        p21_np = np.array(p21)
-        p22_np = np.array(p22)
-        
-        p11_np = p11_np.mean(axis=0)
-        p21_np = p21_np.mean(axis=0)
-        p22_np = p22_np.mean(axis=0)
-        p12_np = p12_np.mean(axis=0)
-        
-        cf = calibration_factor(p11_np, p12_np, p21_np, p22_np)
-        if noise_filter:
-            cf = noise_filtering(cf)
+        path = Path(path)
+        if path.suffix != ".npz":
+            path = path.with_suffix(".npz")
 
-        freqs = fftfreq(f1[0][1].shape[0], 1/f1[0][0])
-        if export == True:
-            base_name = os.path.split(parent_folder)[1]
-            np.save(os.path.join(parent_folder, base_name+"_freqs.npy"), freqs) #freq export
-            np.save(os.path.join(export_folder, base_name+"_cal_f_12.npy"), cf) #cf export
-    return cf, freqs
-
-def transfer_function_from_path(
-        parent_folder: str,
-        noise_filter: bool = False
-        ) -> tuple[np.ndarray, np.ndarray]:
-    """Calculate transfer functions from audio files in the given parent folder.
-
-    Parameters
-    ----------
-    parent_folder : str
-        The path to the parent folder containing the audio files.
-
-    Returns
-    -------
-    tuple
-        A tuple containing the unique values of 'd' extracted from the audio file names,
-        and a list of transfer functions corresponding to each unique 'd' value.
-    """
-    bc_df = load_bc(parent_folder)
-    limit = int(bc_df["lim"].iloc[0])
-    audio_folder = os.path.join(parent_folder, "measurement", "audio")
-    tf_folder = os.path.join(parent_folder, "measurement", "transfer_func")
-    audio_files = read_folder(audio_folder)
-    fs, a = read_file(audio_files[0])
-    limit_idx = int(limit/fftfreq(a.shape[0], 1/fs)[1])
-    audio_files_d_filtered = []
-    for f in audio_files:
-        idx1 = f.rfind("d")
-        idx2 = f.rfind("_")
-        audio_files_d_filtered.append(f[idx1:idx2])
-    unique_d = np.unique(np.array(audio_files_d_filtered))
-    
-    tfs = []
-    for d in unique_d:
-        tf = transfer_function_from_file(folder=audio_folder, filter_str=d)
-        if noise_filter:
-            tf = noise_filtering(tf)
-        tfs.append(tf[:limit_idx])
-        np.save(arr=tfs[-1], file=os.path.join(tf_folder, os.path.split(parent_folder)[1]+"_tf_"+d+".npy"))
-
-    return unique_d, tfs
-
-def alpha_from_path(
-        parent_folder : str, 
-        fs : int=48000, 
-        cal : np.ndarray=None, 
-        tfs : np.ndarray=None, 
-        bc : pd.DataFrame=None, 
-        return_f : bool=False,
-        return_r : bool=False,
-        return_z : bool=False
-        ) -> tuple[np.ndarray]:
-    """Calculate the absorption coefficient from the given parent folder path.
-
-    Parameters
-    ----------
-    parent_folder : str
-        The path of the parent folder.
-    fs : int, optional
-        The sampling frequency, by default 48000.
-    cal : np.ndarray, optional
-        The calibration transfer function, by default None.
-    tfs : np.ndarray, optional
-        The transfer functions, by default None.
-    bc : pd.DataFrame, optional
-        The boundary conditions, by default None.
-    return_f : bool, optional
-        Whether to return the absorption coefficient and frequencies, by default False.
-    return_r : bool, optional
-        Whether to return the reflection factor, by default False.
-    return_z : bool, optional
-        Whether to return the impedance, by default False.
-
-    Returns
-    -------
-    tuple[np.ndarray] or np.ndarray
-        If return_f is True, returns a tuple containing the absorption coefficient and frequencies.
-        Otherwise, returns only the absorption coefficient.
-    """
-    # boundary conditions
-    if bc is None:
-        bc_df = load_bc(parent_folder)
-    else:
-        bc_df = bc
-    temp_c = bc_df["temp"].iloc[0]
-    x1_dist = bc_df["x1"].iloc[0]
-    x2_dist = bc_df["x2"].iloc[0]
-    limit = int(bc_df["lim"].iloc[0])
-    s_dist = x1_dist - x2_dist
-    try:
-        atm_pressure = bc_df["atm_pressure"].iloc[0]
-    except:
-        Exception("atm_pressure not provided with boundary condition file.")
-    if return_z and atm_pressure is None:
-        raise Exception("You need to provide valid atmospheric pressure to calculate surface impedance.")
-    print(f"atm pressure: {atm_pressure}")
-    # read calibration transfer function
-    if cal is None:
-        tf_cal = np.load(
-            read_folder(
-                os.path.join(
-                    parent_folder, "calibration", "cal_factor"
+        with np.load(path, allow_pickle=False) as data:
+            env = None
+            if bool(data["has_env"]):
+                env = EnvironmentalConditions(
+                    temperature=float(data["env_temperature"]),
+                    rel_humidity=float(data["env_rel_humidity"]),
+                    atm_pressure=float(data["env_atm_pressure"]),
                 )
-            )[0]
-        )
-    else:
-        tf_cal = cal
-    freqs = load_freqs(parent_folder)
-    limit_idx = int(limit/freqs[1])
-    tf_cal = tf_cal[:limit_idx]
-    freqs = freqs[:limit_idx]
 
-    # read all transfer functions from folder
-    tf_paths = read_folder(
-        os.path.join(parent_folder, "measurement", "transfer_func")
-    )
-    tf_paths = sorted(tf_paths)
-    # print(tf_paths)
-    if tfs is None:
-        tfs = []
-        for t in tf_paths:
-            tfs.append(np.load(t)[:limit_idx])
+            bc = None
+            if bool(data["has_bc"]):
+                bc = BoundaryConditions(
+                    further_mic_dist=float(data["bc_further_mic_dist"]),
+                    closer_mic_dist=float(data["bc_closer_mic_dist"]),
+                    freq_limit=float(data["bc_freq_limit"]),
+                )
 
-    # calculate transfer function for incident and reflected wave
-    tf_incident, tf_reflected = tf_i_r(temp_c, freqs, s_dist)
-    tf_incident = tf_incident
-    tf_reflected = tf_reflected
-
-    # calculate reflection factor and absorption coefficient
-    rfs = []
-    impedances = []
-    for t in tfs:
-        tf_corrected = t/tf_cal
-        rfs.append(reflection_factor(tf_incident, tf_reflected, tf_corrected, temp_c, freqs, x1_dist))
-    alpha_n = []
-    for r in rfs:
-        alpha_n.append(absorption_coefficient(r))
-    
-    if return_z:
-        for r in rfs:
-            impedances.append(surface_impedance(r, temp_c, atm_pressure))
-    # save all alpha courses
-    for a, p in zip(alpha_n, tf_paths):
-        # print(p)
-        idx = str(p).rfind("d")
-        np.save(arr=a,
-            file=os.path.join(
-                parent_folder, "measurement", "alpha", os.path.split(parent_folder)[1]+"_alpha_"+str(p)[idx:-4]+".npy"
+            return cls(
+                freqs=data["freqs"],
+                alpha=data["alpha"],
+                reflection_factor=(
+                    data["reflection_factor"]
+                    if bool(data["has_reflection_factor"])
+                    else None
+                ),
+                surface_impedance=(
+                    data["surface_impedance"]
+                    if bool(data["has_surface_impedance"])
+                    else None
+                ),
+                env=env,
+                bc=bc,
             )
-        )
-    np.save(arr=freqs, file=os.path.join(
-                parent_folder, "measurement", "alpha", os.path.split(parent_folder)[1]+"_freqs.npy"
-        )
-    )
-
-    # save all reflection factors
-    for r, p in zip(rfs, tf_paths):
-        # print(p)
-        idx = str(p).rfind("d")
-        np.save(arr=r,
-            file=os.path.join(
-                parent_folder, "measurement", "refl_factor", os.path.split(parent_folder)[1]+"_refl_factor_"+str(p)[idx:-4]+".npy"
-            )
-        )
-    np.save(arr=freqs, file=os.path.join(
-                parent_folder, "measurement", "refl_factor", os.path.split(parent_folder)[1]+"_freqs.npy"
-        )
-    )
-
-    # save all impedances
-    for i, p in zip(impedances, tf_paths):
-        # print(p)
-        idx = str(p).rfind("d")
-        np.save(arr=i,
-            file=os.path.join(
-                parent_folder, "measurement", "impedance", os.path.split(parent_folder)[1]+"_impedance_"+str(p)[idx:-4]+".npy"
-            )
-        )
-    np.save(arr=freqs, file=os.path.join(
-                parent_folder, "measurement", "impedance", os.path.split(parent_folder)[1]+"_freqs.npy"
-        )
-    )
-
-    
-    ret = [alpha_n]
-    if return_f:
-        ret = [alpha_n, freqs]
-    if return_r:
-        ret.append(rfs)
-    if return_z:
-        ret.append(impedances)
-        
-    return tuple(ret)
-
-def average_audio(
-        parent_folder : str,
-        direct_folder : str = None,
-        ) -> None:
-    """Average audio files obtained for each variant of 'd' value.
-    'd' value originaly represented cavity depth in resonator measurement,
-    but can be used as a general identifier for different measurement conditions.
-
-    By incorporating this function, the user can do extensive measurements 
-    while keeping the data comsumption low.
-    
-    Parameters
-    ----------
-    parent_folder : str
-        The path to the parent folder of the measurement.
-    direct_folder : str, optional
-        The direct path to the audio folder, by default None.
-    """
-    if direct_folder is None:
-        audio_folder = os.path.join(parent_folder, "measurement", "audio")
-    else:
-        audio_folder = direct_folder
-
-    
-    audio_files = read_folder(audio_folder)
-
-    audio_files_d_filtered = []
-    for f in audio_files:
-        idx1 = f.rfind("d")
-        idx2 = f.rfind("_")
-        audio_files_d_filtered.append(f[idx1:idx2])
-    unique_d = np.unique(np.array(audio_files_d_filtered))
-
-    for d in unique_d:
-        files = filter(audio_files, [d])
-        audio = []
-        for f in files:
-            audio.append(read_file(f)[1])
-
-        audio_np = np.array(audio)
-        print(audio_np.shape)
-        audio_np = audio_np.mean(axis=0)
-        print(audio_np.shape)
-        fs = read_file(files[0])[0]
-
-        wav.write(
-            files[0],
-            fs,
-            audio_np
-        )
-
-        # remove other files
-        for f in files[1:]:
-            os.remove(f)
